b5b3133fb2e5046ec77595367c0a45ea81472d20
okuzmychov <o.kuzmichev@gmail.com>
2023-07-25 22:32:37 +0300

    2

--- src/cat-api.js
+++ src/cat-api.js
@@ -1,15 +1,36 @@
+import Notiflix from 'notiflix';
+
 import axios from 'axios';
 axios.defaults.headers.common['x-api-key'] =
   'live_hpDxNWtuIHgI2hQ0umNyMz8MUXCLnpLObNOexE0JM5utYSQTKqi74y84ybiTShfK';
 axios.defaults.baseURL = 'https://api.thecatapi.com/v1/';
 
-export function fetchBreeds() {
-  return axios.get('breeds').then(({ data }) => {
-    return data;
-  });
+function fetchBreeds() {
+  return axios
+    .get(`breeds/`)
+    .then(resp => {
+      if (resp.status !== 200) {
+        throw new Error(resp.status);
+      }
+      return resp.data;
+    })
+    .catch(() => {
+      Notiflix.Report.failure('Щось пішло не так. Маємо помилку в catch!');
+    });
 }
-export function fetchCatByBreed(breedId) {
-  return axios.get(`images/search?breed_ids=${breedId}`).then(({ data }) => {
-    return data;
-  });
+
+function fetchCatByBreed(breedId) {
+  return axios
+    .get(`/images/search?breed_ids=${breedId}`)
+    .then(resp => {
+      if (resp.status !== 200) {
+        throw new Error(resp.status);
+      }
+      return resp.data[0];
+    })
+    .catch(() => {
+      Notiflix.Report.failure('Щось пішло не так. Маємо помилку в catch!');
+    });
 }
+
+export { fetchBreeds, fetchCatByBreed };
--- src/index.js
+++ src/index.js
@@ -1,77 +1,97 @@
-import { fetchBreeds, fetchCatByBreed } from './cat-api';
-import axios from 'axios';
+import Notiflix from 'notiflix';
 import SlimSelect from 'slim-select';
 import 'slim-select/dist/slimselect.css';
-import Notiflix from 'notiflix';
+import { fetchBreeds, fetchCatByBreed } from './cat-api';
 
-document.addEventListener('DOMContentLoaded', () => {
-  const catInfoDiv = document.querySelector('.cat-info.container');
-  const errorText = document.querySelector('.error');
-  const loaderText = document.querySelector('.loader');
-  const select = document.querySelector('.breed-select');
-  const delay = 2000;
-  // errorText.hidden = true;
-  select.style.display = 'none';
+const breedSelect = document.querySelector('.breed-select');
+const errorText = document.querySelector('.error');
+const loaderText = document.querySelector('.loader');
+const catInfo = document.querySelector('.cat-info');
+const delay = 2000;
 
+breedSelect.style.display = 'none';
 
-  axios.defaults.headers.common['x-api-key'] =
-    'live_hpDxNWtuIHgI2hQ0umNyMz8MUXCLnpLObNOexE0JM5utYSQTKqi74y84ybiTShfK';
-  axios.defaults.baseURL = 'https://api.thecatapi.com/v1/';
-
- Notiflix.Loading.custom('Завантажую...', {
+Notiflix.Loading.custom('Завантажую...', {
   customSvgUrl:
     'https://notiflix.github.io/content/media/loading/notiflix-loading-nx-light.svg',
 });
+
 Notiflix.Loading.remove(delay);
+
+window.addEventListener('load', () => {
+  setTimeout(() => {
+    breedSelect.style.display = 'block';
+  }, delay);
+});
+breedSelect.innerHTML = '<option disabled selected>Виберіть породу</option>';
+let slimSelect;
+
+fetchBreeds()
+  .then(data => {
+    console.log('Масив котів: ', data);
+    const option = data
+      .map(
+        ({ id, name }) => `
+        <option value="${id}">${name}</option>
+    `
+      )
+      .join('');
+    errorText.style.display = 'none';
+    loaderText.style.display = 'none';
+    breedSelect.insertAdjacentHTML('beforeend', option);
+
+    slimSelect = new SlimSelect({
+      select: breedSelect,
+        placeholder: 'Виберіть породу',
+        showSearch: false
+    });
+  })
   
-  fetchBreeds()
-    .then(response => {
-      select.hidden = false;
-      select.innerHTML = response
-        .map(({ id, name }) => {
-          return `<option class="option" value="${id}">${name}</option>`;
-        })
-        .join('');
-        select.style.display = 'block';
-        slimSelect = new SlimSelect({
-        select: '#single',
-        settings: {
-          showSearch: false,
-          searchText: 'Sorry nothing to see here',
-          searchPlaceholder: 'Search for the good stuff!',
-          searchHighlight: true,
-        },
-          events: {
-          afterChange: newVal => {
-            loaderText.hidden = false;
-            catInfoDiv.innerHTML = '';
-            fetchCatByBreed(newVal[0].value)
-              .then(responce => {
-                catInfoDiv.innerHTML = responce
-                  .map(
-                    ({ url, breeds: [{ name, description, temperament }] }) => {
-                      return `
-                        <div class="cat_photo">
-                        <img src="${url}" alt="${name}">
-                        </div>
-                        <div class="cat_descript">
-            <h3>${name}</h3>
-            <p>${description}</p>
-            <p><b>Temperament:&nbsp</b>${temperament}</p></div>`;
-                    }
-                  )
-                  .join('');
-              })
-              .catch(() => {
-                Notiflix.Notify.failure(errorText.textContent);
-              })
-              .finally(() => (loaderText.hidden = true));
-          },
-        },
-      });
+
+  .catch(() => {
+    Notiflix.Report.failure('Щось пішло не так! Перезавантажте сторінку!');
+  });
+
+breedSelect.addEventListener('change', evt => {
+  evt.preventDefault();
+
+  Notiflix.Loading.standard('Завантажую ...');
+
+  const breedSelectId = breedSelect.value;
+
+  fetchCatByBreed(breedSelectId)
+    .then(cat => {
+      console.log(
+        `Інформація про кота з ID=${breedSelectId} та іменем=${cat.breeds[0].name}: `,
+        cat
+      );
+      Notiflix.Loading.remove(delay);
+      const markup = `
+        <div class="container" style="display: flex;">
+
+            <div class="thumb-pic">
+                <img src="${cat.url}" alt="${cat.id}" width="400" />
+            </div>
+
+            <div class="thumb" style="margin-left: 10px; margin-top: -24px; width: 400px;">
+                <h2>${cat.breeds[0].name}</h2>
+                <p>${cat.breeds[0].description}</p>
+                <p><b>Temperament:</b> ${cat.breeds[0].temperament}</p>
+            </div>
+
+        </div>`;
+      setTimeout(() => {
+        catInfo.innerHTML = markup;
+      }, delay);
     })
     .catch(() => {
-      Notiflix.Notify.failure(errorText.textContent);
-    })
-    .finally(() => (loaderText.hidden = true));
+      Notiflix.Report.failure(
+        'Немає інформації про цього кота!',
+        '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Будь ласка оберіть іншу породу!',
+        'Okay'
+      );
+      const markup = ``;
+      catInfo.innerHTML = markup;
+      Notiflix.Loading.remove();
+    });
 });
